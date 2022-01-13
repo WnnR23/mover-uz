33ecad37466290f0a12fb795fc8adeb61d5707d0
WnnR23 <academywnnr@gmail.com>
2022-01-13 21:04:11 +0500

    add js section

--- js/main.js
+++ js/main.js
@@ -1,6 +1,16 @@
-const elDropdownMenuActivator = document.querySelectorAll(".dropdown__menu__activator");
-const elDropdownMenuActive = document.querySelectorAll(".dropdown__menu__active");
-const elDropdownMenu = document.querySelectorAll(".dropdown__menu");
-elDropdownMenuActivator.addEventListener("click", function () {
-  document.DropdownMenu.classList.toggle("DropdownMenuActive")
+const elDropdownMenuActivator = document.querySelector(".dropdown__menu__activator");
+const elDropdownMenu = document.querySelector(".dropdown__menu");
+elDropdownMenuActivator.addEventListener("mouseover", function () {
+  elDropdownMenu.classList.toggle("dropdown__menu__active")
 });
+// elDropdownMenuActivator.addEventListener("mouseleave", function () {
+//   elDropdownMenu.classList.toggle("dropdown__menu__close")
+// });
+
+
+// const elDropdownMenuActivator = document.querySelector(".dropdown__menu__activator");
+// const elDropdownMenuActive = document.querySelector(".dropdown__menu__active");
+// const elDropdownMenu = document.querySelector(".dropdown__menu");
+// elDropdownMenuActivator.addEventListener("click", function () {
+//   elDropdownMenu.classList.toggle("dropdown__menu__active")
+// });
